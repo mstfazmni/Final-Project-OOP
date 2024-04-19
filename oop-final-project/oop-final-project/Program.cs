689f087f7e9b402b85b20586e3ad9b19f3fae049
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2024-04-19 13:40:58 -0600

    Coordinate class

---
+++ oop-final-project/oop-final-project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Coordinate
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+}
